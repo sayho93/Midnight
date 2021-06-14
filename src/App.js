0102e2069e8ff43cf9f7d3757776046be8ac0c41
sayhoChun <fishcreek@naver.com>
2021-06-15 00:20:54 +0900

    react-spring added & login api call added

--- src/App.js
+++ src/App.js
@@ -17,9 +17,16 @@ import NetUtil from './utils/NetUtil';
 
 import {useSpring, animated} from 'react-spring';
 
+const AnimatedView = animated(View);
+
 const App: () => Node = () => {
     const [state, setToggleState] = useState({
         toggle: false,
+    });
+
+    const [loginInfo, setLoginInfo] = useState({
+        email: '',
+        pw: '',
     });
 
     const isDarkMode = useColorScheme() === 'dark';
@@ -30,29 +37,38 @@ const App: () => Node = () => {
         height: '100%',
     };
 
-    async function onLoginPress() {
-        const isLoggedIn = await NetUtil.login('ellivga@dongguk.edu', 'fishcreek');
-        if (isLoggedIn === true) {
-            alert('Success, redirect to Details');
-            // navigation.navigate('Details');
-        }
-    }
-
-    async function onJoinPress() {
-        await NetUtil.getBoard(136);
-    }
-
     const toggleHandler = () => {
         setToggleState({
             toggle: !state.toggle,
         });
     };
 
-    const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } })
+    const emailHandler = text => {
+        setLoginInfo({email: text, pw: loginInfo.pw});
+    };
+
+    const pwHandler = text => {
+        setLoginInfo({email: loginInfo.email, pw: text});
+    };
 
     let group = null;
-    if (state.toggle) group = <InputGroup onLoginPress={onLoginPress} />;
-    else group = <ButtonGroup onLoginPress={toggleHandler} onJoinPress={onJoinPress} />;
+    if (state.toggle) {
+        group = <InputGroup onLoginPress={onLoginPress} emailHandler={emailHandler} pwHandler={pwHandler} />;
+    } else {
+        group = <ButtonGroup onLoginPress={toggleHandler} onJoinPress={onJoinPress} />;
+    }
+
+    async function onJoinPress() {
+        await NetUtil.getBoard(136);
+    }
+
+    async function onLoginPress() {
+        console.log(loginInfo);
+        const user = await NetUtil.login(loginInfo.email, loginInfo.pw);
+        if (user) {
+            console.log(user);
+        }
+    }
 
     return (
         <SafeAreaView style={backgroundStyle}>
@@ -71,38 +87,44 @@ const App: () => Node = () => {
     );
 };
 
-class InputGroup extends Component {
-    constructor(props) {
-        super(props);
-    }
+const InputGroup = props => {
+    const animation = useSpring({
+        to: {opacity: 1},
+        from: {opacity: 0},
+        delay: 100,
+    });
 
-    render() {
-        return (
+    return (
+        <AnimatedView style={animation}>
             <View style={styles.groupLayout}>
                 <View style={styles.inputGroup}>
                     <TextInput
                         style={styles.input}
+                        onChangeText={text => props.emailHandler(text)}
                         placeholder="이메일"
                         placeholderTextColor={ValueConst.colors.thisgray}
                     />
                     <TextInput
                         style={styles.input}
+                        onChangeText={text => props.pwHandler(text)}
                         textContentType="password"
-                        keyboardType="visible-password"
                         placeholder="패스워드"
+                        returnKeyType="go"
+                        secureTextEntry={true}
+                        autoCorrect={false}
                         placeholderTextColor={ValueConst.colors.thisgray}
                     />
                     <CustomButton
                         title="로그인"
-                        onPress={this.props.onLoginPress}
+                        onPress={props.onLoginPress}
                         color={ValueConst.colors.colorPrimaryDark}
                         textColor={ValueConst.colors.white}
                     />
                 </View>
             </View>
-        );
-    }
-}
+        </AnimatedView>
+    );
+};
 
 class ButtonGroup extends Component {
     constructor(props) {
@@ -138,7 +160,6 @@ const styles = StyleSheet.create({
         marginBottom: 50,
         justifyContent: 'center',
         alignItems: 'center',
-        // backgroundColor: 'red',
     },
     lottie: {
         width: 180,
@@ -153,13 +174,11 @@ const styles = StyleSheet.create({
     groupLayout: {
         margin: 20,
         alignItems: 'center',
-        // backgroundColor: 'black',
     },
     btnGroup: {
         width: '100%',
         padding: 20,
         alignItems: 'center',
-        // backgroundColor: ValueConst.colors.colorPrimaryDark,
     },
     bottomLogo: {
         marginTop: 40,
