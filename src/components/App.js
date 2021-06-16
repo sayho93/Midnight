cadeba2c812fe6f11b21be2318a0c80bdbebeaad
sayhoChun <fishcreek@naver.com>
2021-06-17 05:05:28 +0900

    native-stack done

--- src/components/App.js
+++ src/components/App.js
@@ -7,55 +7,22 @@
  */
 import 'react-native-gesture-handler'
 
-import React, {useState} from 'react'
-import type {Node} from 'react'
-import {
-    Platform,
-    SafeAreaView,
-    StatusBar,
-    StyleSheet,
-    Image,
-    useColorScheme,
-    View,
-    Alert,
-    ToastAndroid,
-    KeyboardAvoidingView,
-    TouchableOpacity,
-    Text,
-    TextInput,
-} from 'react-native'
-
-import NetUtil from '../utils/NetUtil'
-import LottieView from 'lottie-react-native'
-import ValueConst from '../utils/ValueConst'
-import ButtonGroup from './auth/ButtonGroup'
-import {InputGroup} from './auth/InputGroup'
+import React from 'react'
+import {View, TouchableOpacity, Text} from 'react-native'
 import PropTypes from 'prop-types'
 import {enableScreens} from 'react-native-screens'
 import {createNativeStackNavigator} from 'react-native-screens/native-stack'
-import {CustomStatusBar} from './CustomStatusBar'
+import EntryPoint from '../screens/EntryPoint'
 
 enableScreens()
 const Stack = createNativeStackNavigator()
 
-function HomeScreen({navigation}) {
-    return (
-        <TouchableOpacity
-            onPress={() => {
-                // Alert.alert('test')
-                console.log('clicked')
-                navigation.navigate('DetailsScreen')
-            }}
-            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Go To Detail Screen</Text>
-        </TouchableOpacity>
-    )
-}
-
-function DetailsScreen() {
+function DetailsScreen({navigation}) {
     return (
         <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>Detail Screen</Text>
+            <TouchableOpacity onPress={() => navigation.navigate('Detail', {count: 1})}>
+                <Text>Detail Screen</Text>
+            </TouchableOpacity>
         </View>
     )
 }
@@ -77,103 +44,17 @@ const Detail = ({route, navigation}) => {
 }
 
 const App = () => {
-    const [state, setToggleState] = useState({
-        toggle: false,
-    })
-
-    const [loginInfo, setLoginInfo] = useState({
-        email: '',
-        pw: '',
-    })
-
-    const isDarkMode = useColorScheme() === 'dark'
-
-    const backgroundStyle = {
-        // backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-        backgroundColor: ValueConst.colors.themeColor,
-        height: '100%',
-    }
-
-    const toggleHandler = () => {
-        setToggleState({
-            toggle: !state.toggle,
-        })
-    }
-
-    const emailHandler = text => {
-        setLoginInfo({email: text, pw: loginInfo.pw})
-    }
-
-    const pwHandler = text => {
-        setLoginInfo({email: loginInfo.email, pw: text})
-    }
-
-    let group = null
-    if (state.toggle)
-        group = <InputGroup onLoginPress={onLoginPress} emailHandler={emailHandler} pwHandler={pwHandler} />
-    else group = <ButtonGroup onLoginPress={toggleHandler} onJoinPress={onJoinPress} />
-
-    async function onJoinPress() {
-        await NetUtil.getBoard(136).then(res => {
-            Alert.alert(res.data.content)
-        })
-    }
-
-    async function onLoginPress() {
-        await NetUtil.login(loginInfo.email, loginInfo.pw).then(res => {
-            if (res.returnCode !== 1) {
-                if (Platform.OS === 'android') ToastAndroid.show(res.returnMessage, ToastAndroid.SHORT)
-                else Alert.alert(res.returnMessage)
-            } else {
-                if (Platform.OS === 'android') ToastAndroid.show(res.returnMessage, ToastAndroid.SHORT)
-                else Alert.alert(res.returnMessage)
-            }
-        })
-    }
-
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} initialParams={{itemId: 42}} />
+            <Stack.Screen
+                name="EntryPoint"
+                component={EntryPoint}
+                initialParams={{itemId: 42}}
+                options={{headerShown: false}}
+            />
             <Stack.Screen name="Detail" component={Detail} />
             <Stack.Screen name="DetailsScreen" component={DetailsScreen} />
         </Stack.Navigator>
-
-        // <NavigationContainer>
-        //     <Stack.Navigator
-        //         screenOptions={{
-        //             headerStyle: {
-        //                 backgroundColor: '#f4511e',
-        //             },
-        //             headerTintColor: '#fff',
-        //             headerTitleStyle: {
-        //                 fontWeight: 'bold',
-        //             },
-        //         }}>
-        //         <Stack.Screen
-        //             name="SignIn"
-        //             component={CustomStatusBar}
-        //             options={{
-        //                 headerShown: false,
-        //             }}
-        //         />
-        //         <Stack.Screen name="SignUp" component={ButtonGroup} options={{headerBackTitleVisible: false}} />
-        //     </Stack.Navigator>
-        //     <SafeAreaView style={backgroundStyle}>
-        //         <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-        //         <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : 'height'}>
-        //             <View style={styles.logoLayout}>
-        //                 <LottieView
-        //                     source={require('../../assets/animations/love-explosion.json')}
-        //                     style={styles.lottie}
-        //                     autoPlay
-        //                     loop
-        //                 />
-        //                 <Image style={styles.logo} source={require('../../assets/img/title_logo_small.png')} />
-        //             </View>
-        //             {group}
-        //         </KeyboardAvoidingView>
-        //     </SafeAreaView>
-        // </NavigationContainer>
     )
 }
 
@@ -196,24 +77,5 @@ App.defaultProps = {
     handleAddCounter: () => console.warn('handleAddCounter not defiend'),
     handleRemoveCounter: () => console.warn('handleRemoveCounter not defiend'),
 }
-
-const styles = StyleSheet.create({
-    logoLayout: {
-        marginTop: 100,
-        marginBottom: 50,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    lottie: {
-        width: 180,
-        height: 180,
-        marginBottom: 20,
-    },
-    logo: {
-        resizeMode: 'cover',
-        width: 150,
-        height: 45,
-    },
-})
 
 export default App
---
+++ src/screens/EntryPoint.js
@@ -0,0 +1,122 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler'
+
+import React, {useState} from 'react'
+import {
+    Platform,
+    SafeAreaView,
+    StatusBar,
+    StyleSheet,
+    Image,
+    useColorScheme,
+    View,
+    Alert,
+    ToastAndroid,
+    KeyboardAvoidingView,
+} from 'react-native'
+
+import NetUtil from '../utils/NetUtil'
+import LottieView from 'lottie-react-native'
+import ValueConst from '../utils/ValueConst'
+import ButtonGroup from '../components/auth/ButtonGroup'
+import {InputGroup} from '../components/auth/InputGroup'
+import PropTypes from 'prop-types'
+
+const EntryPoint = ({route, navigation}) => {
+    const [state, setToggleState] = useState({
+        toggle: false,
+    })
+
+    const [loginInfo, setLoginInfo] = useState({
+        email: '',
+        pw: '',
+    })
+
+    const isDarkMode = useColorScheme() === 'dark'
+
+    const backgroundStyle = {
+        // backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        backgroundColor: ValueConst.colors.themeColor,
+        height: '100%',
+    }
+
+    const toggleHandler = () => {
+        setToggleState({
+            toggle: !state.toggle,
+        })
+    }
+
+    const emailHandler = text => {
+        setLoginInfo({email: text, pw: loginInfo.pw})
+    }
+
+    const pwHandler = text => {
+        setLoginInfo({email: loginInfo.email, pw: text})
+    }
+
+    let group = null
+    if (state.toggle)
+        group = <InputGroup onLoginPress={onLoginPress} emailHandler={emailHandler} pwHandler={pwHandler} />
+    else group = <ButtonGroup onLoginPress={toggleHandler} onJoinPress={onJoinPress} />
+
+    function onJoinPress() {
+        navigation.navigate('DetailsScreen')
+    }
+
+    async function onLoginPress() {
+        await NetUtil.login(loginInfo.email, loginInfo.pw).then(res => {
+            if (res.returnCode !== 1) {
+                if (Platform.OS === 'android') ToastAndroid.show(res.returnMessage, ToastAndroid.SHORT)
+                else Alert.alert(res.returnMessage)
+            } else {
+                if (Platform.OS === 'android') ToastAndroid.show(res.returnMessage, ToastAndroid.SHORT)
+                else Alert.alert(res.returnMessage)
+            }
+        })
+    }
+
+    return (
+        <SafeAreaView style={backgroundStyle}>
+            <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+            <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'position' : 'height'}>
+                <View style={styles.logoLayout}>
+                    <LottieView
+                        source={require('../../assets/animations/love-explosion.json')}
+                        style={styles.lottie}
+                        autoPlay
+                        loop
+                    />
+                    <Image style={styles.logo} source={require('../../assets/img/title_logo_small.png')} />
+                </View>
+                {group}
+            </KeyboardAvoidingView>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    logoLayout: {
+        marginTop: 100,
+        marginBottom: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    lottie: {
+        width: 180,
+        height: 180,
+        marginBottom: 20,
+    },
+    logo: {
+        resizeMode: 'cover',
+        width: 150,
+        height: 45,
+    },
+})
+
+export default EntryPoint
