6b20c2842b383189758b6a8507196868b0096d95
sayhoChun <fishcreek@naver.com>
2021-06-23 19:50:23 +0900

    working on splash screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import AuthNavigator from 'pages/auth/AuthNavigator'
 import {Provider} from 'react-redux'
 import {NavigationContainer} from '@react-navigation/native'
 import Store from 'store/Store'
+import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
+    useEffect(() => {
+        SplashScreen.hide()
+    }, [])
+
     return (
         <Provider store={Store}>
             <NavigationContainer>
