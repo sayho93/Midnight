0e85982bc00a2a73949fac14a45de5f6dd036abc
sayhoChun <fishcreek@naver.com>
2021-06-24 16:42:09 +0900

    expo setting for Android/IOS done

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import AuthNavigator from 'pages/auth/AuthNavigator'
 import {Provider} from 'react-redux'
 import {NavigationContainer} from '@react-navigation/native'
 import Store from 'store/Store'
-import SplashScreen from 'react-native-splash-screen'
 
 const App = () => {
-    useEffect(() => {
-        SplashScreen.hide()
-    }, [])
-
     return (
         <Provider store={Store}>
             <NavigationContainer>
